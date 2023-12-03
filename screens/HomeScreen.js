1b4179e30f5d361e5e94b4d58b0e68cc5dae30a0
tjaos <th83287716@gmail.com>
2023-12-03 17:17:13 -0300

    Dinamizando algumas informações

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,33 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { Icon } from "react-native-elements/dist/icons/Icon";
 import { Image } from "react-native-elements";
-/*
-const deliveryImage = Image.resolveAssetSource(DeliveryImage).uri;
-*/
+import { TouchableOpacity } from "react-native";
+import Modal from "react-native-modal";
+
 export default function HomeScreen() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
+
+  const handleCategoryPress = (category) => {
+    setSelectedCategory(category);
+    toggleModal();
+  };
+
+  const isCategorySelected = (category) => {
+    return category === selectedCategory;
+  };
+  const burgers = [
+    { name: "Creesse Burger", price: "R$ 40" },
+    { name: "Chezzy Burger", price: "R$ 35" },
+    { name: "Misto", price: "R$ 14" },
+    { name: "Sanduba bom", price: "R$ 23" },
+  ];
+
   return (
     <View style={styles.container}>
       <View style={styles.background}>
@@ -35,10 +57,58 @@ export default function HomeScreen() {
         </View>
         <View style={styles.content}>
           <View style={styles.topics}>
-            <Text>Burger</Text>
-            <Text>Pizza</Text>
-            <Text>Pasta</Text>
-            <Text>Popular</Text>
+            <TouchableOpacity
+              onPress={() => handleCategoryPress("burger")}
+              style={[
+                styles.category,
+                isCategorySelected("burger") && {
+                  backgroundColor: "#9377C6",
+                  borderRadius: 14,
+                  padding: 5,
+                },
+              ]}
+            >
+              <Text>Burger</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCategoryPress("pizza")}
+              style={[
+                styles.category,
+                isCategorySelected("pizza") && {
+                  backgroundColor: "#9377C6",
+                  borderRadius: 14,
+                  padding: 5,
+                },
+              ]}
+            >
+              <Text>Pizza</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCategoryPress("pasta")}
+              style={[
+                styles.category,
+                isCategorySelected("pasta") && {
+                  backgroundColor: "#9377C6",
+                  borderRadius: 14,
+                  padding: 5,
+                },
+              ]}
+            >
+              <Text>Pasta</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => handleCategoryPress("popular")}
+              style={[
+                styles.category,
+                isCategorySelected("popular") && {
+                  backgroundColor: "#9377C6",
+                  borderRadius: 14,
+                  padding: 5,
+                },
+              ]}
+            >
+              <Text>Popular</Text>
+            </TouchableOpacity>
           </View>
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
@@ -49,7 +119,7 @@ export default function HomeScreen() {
               </Text>
               <Text style={{ fontSize: 12, color: "gray" }}>Restaurent</Text>
             </View>
-            <View
+            <Text
               style={{
                 paddingRight: 15,
                 height: 40,
@@ -62,104 +132,82 @@ export default function HomeScreen() {
               }}
             >
               2/10
-            </View>
+            </Text>
           </View>
           <View style={styles.ListItems}>
-            <View style={styles.item}>
-              <Image
-                source={require("../assets/image/cheesseburger.png")}
-                style={{
-                  width: 80,
-                  height: 80,
-                  paddingTop: 0,
-                }}
-              />
-              <Text style={{ fontWeight: "bold", color: "#dcdcdc" }}>
-                Creesse Burger
-              </Text>
-              <View style={{ flexDirection: "row" }}>
-                <Icon 
-                name="star" 
-                size={25} 
-                color={"white"} />
-                <Icon 
-                name="star" 
-                size={25} 
-                color={"white"} />
-                <Icon 
-                name="star" 
-                size={25} 
-                color={"white"} />
-                <Icon 
-                name="star-half" 
-                size={25} 
-                color={"white"} />
-                <Icon name="star" size={25} style={{}} />
-              </View>
-              <Text
-                style={{
-                  backgroundColor: "white",
-                  padding: 2,
-                  borderRadius: 5,
-                }}
-              >
-                R$ 40{" "}
-              </Text>
-            </View>
-            <View style={styles.item}>
-              <Image
-                source={require("../assets/image/cheesyburger.png")}
-                style={{
-                  width: 80,
-                  height: 80,
-                  paddingTop: 0,
-                }}
-              />
-
-              <Text style={{ fontWeight: "bold", color: "#dcdcdc" }}>
-                Chezzy Burger
-              </Text>
-              <View style={{ flexDirection: "row" }}>
-                <Icon 
-                name="star" 
-                size={25} 
-                color={"white"} />
-                <Icon 
-                name="star" 
-                size={25} 
-                color={"white"} />
-                <Icon 
-                name="star" 
-                size={25} 
-                color={"white"} />
-                <Icon 
-                name="star-half" 
-                size={25} 
-                color={"white"} />
-                <Icon 
-                name="star" 
-                size={25} 
-                style={{}} />
-              </View>
-              <Text
-                style={{
-                  backgroundColor: "white",
-                  padding: 2,
-                  borderRadius: 5,
-                }}
-              >
-                R$ 35{" "}
-              </Text>
-            </View>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+              {burgers.map((burger, index) => (
+                <View key={index} style={styles.item}>
+                  <TouchableOpacity
+                    onPress={() => handleCategoryPress(burger.name)}
+                  >
+                    <Image
+                      source={require("../assets/image/cheesseburger.png")}
+                      style={{
+                        width: 80,
+                        height: 80,
+                        paddingTop: 0,
+                      }}
+                    />
+                    <Text style={{ fontWeight: "bold", color: "#dcdcdc" }}>
+                      {burger.name}
+                    </Text>
+                    <View style={{ flexDirection: "row" }}>
+                      <Icon name="star" size={25} color={"white"} />
+                      <Icon name="star" size={25} color={"white"} />
+                      <Icon name="star" size={25} color={"white"} />
+                      <Icon name="star-half" size={25} color={"white"} />
+                      <Icon name="star" size={25} style={{}} />
+                    </View>
+                    <Text
+                      style={{
+                        backgroundColor: "white",
+                        padding: 2,
+                        borderRadius: 5,
+                      }}
+                    >
+                      {burger.price}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              ))}
+            </ScrollView>
           </View>
           <View style={styles.navbar}>
-            <View>Home</View>
-            <View>Lupa</View>
-            <View>Sino</View>
-            <View>User</View>
+            <TouchableOpacity>
+              <Icon name="home" size={25} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon name="search" size={25} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon name="notifications" size={25} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon name="person" size={25} />
+            </TouchableOpacity>
           </View>
         </View>
       </View>
+      {/* Modal */}
+      <Modal isVisible={isModalVisible} style={{ margin: 0 }} onBackdropPress={() => setModalVisible(false)}>
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10 }}>
+      <Text>Quantidade: 1</Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+        <TouchableOpacity>
+          <Text style={{ fontSize: 20, color: 'green' }}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={{ fontSize: 20, color: 'red' }}>-</Text>
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={{ backgroundColor: 'blue', padding: 10, borderRadius: 5, marginTop: 10 }}>
+        <Text style={{ color: 'white', textAlign: 'center' }}>Add to bag</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+</Modal>
     </View>
   );
 }
@@ -214,11 +262,16 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "flex-start",
     flexDirection: "column",
+    marginRight: 10,
   },
   navbar: {
     justifyContent: "space-between",
     flexDirection: "row",
     alignItems: "center",
     padding: 20,
+  },
+  category: {
+    padding: 10,
+    borderRadius: 10,
   },
 });
